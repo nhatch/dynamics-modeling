75d31c7d04308409df51f716c5c48a630c43a761
Jakub Filipek <balbok@cs.washington.edu>
2022-05-16 19:03:21 -0700

    Modify look at dists to save visualizations

--- examples/look_at_dists.py
+++ examples/look_at_dists.py
@@ -1,11 +1,13 @@
 import numpy as np
 from rosbag2torch import readers, filters, load_bags
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 
 
 def main():
-    DATASET_TRAIN = "datasets/rzr_real"
+    DATASET_TRAIN = "datasets/rzr_real_backward"
+    dataset_name = DATASET_TRAIN.split("/")[-1]
 
     features = ["control", "state"]
     delayed_features = ["target"]
@@ -15,7 +17,7 @@ def main():
         list(set(features + delayed_features)),
         features_to_record_on=["control"],
         filters=[
-            filters.ForwardFilter(),
+            filters.BackwardFilter(),
             filters.PIDInfoFilter()
         ]
     )
@@ -33,6 +35,8 @@ def main():
 
     train_sequences = load_bags(DATASET_TRAIN, train_reader)
 
+    # print(train_sequences)
+
     state_dx = []
     state_dtheta = []
 
@@ -44,29 +48,42 @@ def main():
         state_dx.extend(sequence["state"][:, 0])
         state_dtheta.extend(sequence["state"][:, 1])
 
-        control_steer.extend(sequence["control"][:, 0])
-        control_throttle.extend(sequence["control"][:, 1])
-        control_brake.extend(sequence["control"][:, 2])
+        control_throttle.extend(sequence["control"][:, 0])
+        control_brake.extend(sequence["control"][:, 1])
+        control_steer.extend(sequence["control"][:, 2])
 
     control_throttle_brake_combined = np.array(control_throttle) - np.array(control_brake)
 
-    plt.hist2d(control_throttle, control_brake, bins=100)
-    plt.xlabel("Throttle")
-    plt.ylabel("Brake")
-    plt.show()
+    if not Path(f"plots/{dataset_name}").exists():
+        Path(f"plots/{dataset_name}").mkdir()
 
     for title, data in [
-        # ("state_dx", state_dx),
-        # ("state_dtheta", state_dtheta),
-        # ("control_steer", control_steer),
+        ("state_dx", state_dx),
+        ("state_dtheta", state_dtheta),
+        ("control_steer", control_steer),
         ("control_throttle", control_throttle),
-        # ("control_brake", control_brake),
+        ("control_brake", control_brake),
         ("control_throttle_brake_combined", control_throttle_brake_combined),
     ]:
         plt.figure()
         plt.title(f"{title} - {np.mean(data):.4g}")
         plt.hist(data)
-        plt.show()
+        plt.savefig(f"plots/{dataset_name}/hist_{title}.png", bbox_inches="tight")
+
+        plt.cla()
+        plt.clf()
+
+    for label_x, data_x, label_y, data_y in [
+        ("Throttle", control_throttle, "Brake", control_brake),
+    ]:
+        plt.figure()
+        plt.hist2d(data_x, data_y)
+        plt.xlabel(label_x)
+        plt.ylabel(label_y)
+        plt.savefig(f"plots/{dataset_name}/hist2d_{label_x}_{label_y}.png", bbox_inches="tight")
+
+        plt.cla()
+        plt.clf()
 
 
 if __name__ == "__main__":
